880505ad3ca138f1bf8ecce32d449ea44d303c66
bulbul <bulbulzetra93@gmail.com>
2023-07-06 11:37:53 +0600

    firebase config added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -16,6 +16,7 @@
 // // Initialize Firebase
 // export const app = initializeApp(firebaseConfig);
 
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
